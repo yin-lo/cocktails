46d80c511415d6561232bd37436414d2d56efb93
yin-lo <yinlo@hotmail.fr>
2024-03-02 16:45:30 +0100

    fin du challenge avec bonus image

--- data/recipes.js
+++ data/recipes.js
@@ -3,25 +3,29 @@ const recipes = [
     name: "sandwich",
     preparationTime: 10,
     author: "Lord Sandwich",
-    ingredients: ["pain", "fromage", "jambon", "salade"]
+    ingredients: ["pain", "fromage", "jambon", "salade"],
+    image: "/img/sandwich.jpg"
   },
   {
     name: "pancakes",
     preparationTime: 20,
     author: "Lady Cakes",
-    ingredients: ["farine", "oeuf", "sucre", "levure"]
+    ingredients: ["farine", "oeuf", "sucre", "levure"],
+    image: "/img/pancakes.jpg"
   },
   {
     name: "tartiflette",
     preparationTime: 20,
     author: "John Reblochon",
-    ingredients: ["patates", "oignon", "reblochon", "lardon"]
+    ingredients: ["patates", "oignon", "reblochon", "lardon"],
+    image: "/img/tartiflette.jpg"
   },
   {
     name: "guacamole",
     preparationTime: 20,
     author: "Lady Cakes",
-    ingredients: ["avocat", "tomate", "huile", "oignons", "épices"]
+    ingredients: ["avocat", "tomate", "huile", "oignons", "épices"],
+    image: "/img/guacamole.jpg"
   },
 ];
 
--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use(express.static('data'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 	// res.send(`Exemple de recette : ${recipes[0].name}`);
 
-	res.render('recipes.ejs', {recipes});
+	res.render('recipes.ejs', { recipes });
+});
+
+app.get('/recipes/:recipeName', (req, res) => {
+	const foundRecipe = recipes.find((e) => e.name === req.params.recipeName);
+
+	res.render('recipe.ejs', { foundRecipe });
 });
 
 const port = 3000;
